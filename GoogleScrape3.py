4cfe18baa70a360cd9b9a245eabd3b373f0cbe0f
Jason Bell <j.jason.bell@gmail.com>
2015-11-20 09:58:12 -0600

    Added line to remove non-ASCII characters from the scraped text.

--- GoogleScrape3.py
+++ GoogleScrape3.py
@@ -3,19 +3,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import urllib
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import time
 import random
+import requests
 
-start_time = time.time() # For timing the whole operation
+start_time=time.time() # For timing the whole operation
 
-search_text = input("What do you seek? ") # Gives us our goal
-pages = int(input("How many pages of results would you like? "))
+search_text=input("What do you seek? ") # Gives us our goal
+pages=int(input("How many pages of results would you like? "))
 
-browser = webdriver.Firefox() # Establishes which web browser we will be using
+browser= webdriver.Firefox() # Establishes which web browser we will be using
 browser.implicitly_wait(10)
 
 url = 'http://www.google.com' # Target url
@@ -25,33 +25,32 @@ browser.get(url)
 #browser.set_window_size(500,500)
 
 # Find the text input in order to search
-search_bar_id = "lst-ib"
+search_bar_id="lst-ib"
 
 # Complete a search
-elem = browser.find_element_by_id(search_bar_id)
+elem=browser.find_element_by_id(search_bar_id)
 elem.clear()
 elem.send_keys(search_text, Keys.ENTER) # From the beginning input
 
 # Obtain a list of the urls that are listed
-address_book = [] #initialize list
-# for allowing the page to load each time you get a new page of results
-wait = WebDriverWait(browser, 10) 
+address_book=[] #initialize list
+wait = WebDriverWait(browser,10) # for allowing the page to load each time you get a new page of results
 
-# Do this for the first (n) pages  
+# Do this for the first (n) pages
 for n in range(pages):
 
     # Find the 'next page' button and wait until everything is loaded
-    # elem = wait.until(expected_conditions.element_to_be_clickable((By.XPATH,'//a[@id="pnnext"]')))
+    #elem=wait.until(expected_conditions.element_to_be_clickable((By.XPATH,'//a[@id="pnnext"]')))
     
     elems = browser.find_elements_by_xpath("//ol//div//div/div/h3/a")
     for el in elems:
-        url = str(el.get_attribute('href'))
+        url=str(el.get_attribute('href'))
         address_book.append(url)
         
-    elem = browser.find_element_by_xpath('//a[@id="pnnext"]')
+    elem=browser.find_element_by_xpath('//a[@id="pnnext"]')
     if n < (pages - 1):
         elem.click()
-        wait_time = random.random() + 0.6 # So as to be inconspicuous
+        wait_time=random.random()+0.6 # So as to be inconspicuous
         time.sleep(wait_time)
 
 browser.quit()
@@ -61,20 +60,20 @@ print (address_book)
 #address_book=['https://en.wikipedia.org/wiki/Pinochle', 'http://www.games.com/game/masque-publishing/pinochle', 'http://www.pagat.com/marriage/pinmain.html', 'http://www.playok.com/en/pinochle/', 'http://www.bicyclecards.com/how-to-play/pinochle-2/', 'https://play.google.com/store/apps/details?id=com.karmangames.pinochle&hl=en', 'http://www.pogo.com/games/pinochle', 'http://www.britannica.com/topic/pinochle', 'https://itunes.apple.com/us/app/pinochle/id351086172?mt=8', 'http://www.instructables.com/id/How-to-play-double-deck-Pinochle/']
 
 # Compile the text from all the web pages
-fulltext = '' #initialize
+fulltext='' #initialize
 
 for x in range(len(address_book)):
     # Retrieve the html
-    html = urllib.urlopen(address_book[x]).read()
+    r=requests.get(address_book[x])
+    html=r.text
 
     # Remove style elements
-    soup = BeautifulSoup(html,"lxml")
+    soup=BeautifulSoup(html,"lxml")
     for script in soup(["script", "style"]):
         script.extract()
     text = soup.get_text() # get text
 
-    # Found in stack overflow: 
-    # http://stackoverflow.com/questions/22799990/beatifulsoup4-get-text-still-has-javascript
+    # Found in stack overflow: http://stackoverflow.com/questions/22799990/beatifulsoup4-get-text-still-has-javascript
     # BeautifulSoup Documentation: http://www.crummy.com/software/BeautifulSoup/bs4/doc/
     # perhaps not entirely necessary:
 
@@ -84,14 +83,15 @@ for x in range(len(address_book)):
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     # drop blank lines
     text = '\n'.join(chunk for chunk in chunks if chunk)
-    text = text.encode('utf-8')
+##    text=text.encode('utf-8')
 
     fulltext = fulltext + text
 
-with open("utput.txt", "w") as f:
+fulltext = ''.join([i if ord(i) < 128 else ' ' for i in fulltext])
+
+with open("output.txt", "w") as f:
     f.write(fulltext)
 
 print ('Text from %s web pages scanned.' % len(address_book))
-t = time.time() - start_time
+t=time.time()-start_time
 print ('----- %s seconds -----' % t)
-print (t)

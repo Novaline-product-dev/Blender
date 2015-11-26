769e9c460a60b3a5e2559cfc29f9a5d362568529
Jason Bell <j.jason.bell@gmail.com>
2015-11-25 22:14:56 -0600

    Now using latent semantic indexing to suggest the word, scraper modified for compatibility.

--- TextNetwork.py
+++ TextNetwork.py
@@ -8,12 +8,16 @@ import networkx as nx
 import textFunctions
 from gensim import corpora, models, similarities, utils
 
-# The next line throws a warning, but I checked and the sklearn dev team says don't worry about it.
-from sklearn.feature_extraction import text 
+# The next line throws a warning, but I checked and the sklearn dev team 
+# says don't worry about it.
+from sklearn import feature_extraction 
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 
-os.chdir(os.getenv('HOME') + '/Documents/Blender')
+AuxPath = os.getenv('HOME') + '/Documents/Blender/Aux'
+BlenderPath = os.getenv('HOME') + '/Documents/Blender'
+os.chdir(BlenderPath)
+
 textList = pickle.load( open("output.p", "rb"))
 
 stemmer = SnowballStemmer('english')
@@ -27,20 +31,57 @@ for i, doc in enumerate(textList):
 	temp = [w for w in temp if w not in string.punctuation]
 	temp = [textFunctions.rmPunct(w) for w in temp]
 	
-	# remove characters specified by me
+	# remove tokens specified by me
 	temp = [w for w in temp if w not in set(['[', ']', '\'', 
 		'\n', 'com'])]
 
-	# remove stopwords.  nltk is case-sensitive: use lowercase.  This step has to be done last, after removing punctuation, etc.
+	# remove stopwords.  nltk is case-sensitive: use lowercase. 
+	# This step has to be done last, after removing punctuation, etc.
 	temp = [w for w in temp if w not in stopwords.words('english')]
 
-	# stemming is usually done, but in this case we want human readable format. We'll have to explore this issue. 
+	# stemming is usually done, but in this case we want human readable format. 
+	# We'll have to explore this issue. 
 	# temp = [stemmer.stem(w) for w in temp]
 
 	# reversing the split performed above
-	textList[i] = ' '.join(temp)
+	textList[i] = temp
 
-vectorizer = text.CountVectorizer() # initializes a counter from sklearn
+dictionary = corpora.Dictionary(textList) # collects stats for each word
+dictionary.save(AuxPath + '/currentDictionary.dict') # save for later
+
+# This is gensim's style of corpus
+corpus = [dictionary.doc2bow(doc) for doc in textList] 
+# store to disk, for later
+corpora.MmCorpus.serialize(AuxPath + '/textList.mm', corpus) 
+lsi = models.LsiModel(corpus, num_topics = len(textList))
+
+search_text = pickle.load(open('output2.p', 'rb'))
+index = similarities.MatrixSimilarity(lsi[corpus])
+searchList = [set(text) for text in textList]
+simList = []
+wordList = []
+for text in searchList:
+	for word in text:
+		wordList.extend([word])
+		phrase = search_text + ' ' + word
+		vec_repr = dictionary.doc2bow(phrase.split())
+		vec_lsi = lsi[vec_repr] # convert the query to LSI space
+		sim = sum(index[vec_lsi])
+		simList.extend([sim])
+
+# new stimulus word
+print(wordList[np.argmin(np.absolute(simList))]) 
+
+
+
+
+# Below is just for plotting.  Comment out if you don't want the plot
+#-------------------------------------------------------------------------------
+for i, text in enumerate(textList):
+	textList[i] = ' '.join(text)
+
+# initializes a counter from sklearn
+vectorizer = feature_extraction.text.CountVectorizer() 
 
 # the counter creates a dtm from textList 
 dtm = vectorizer.fit_transform(textList) 
@@ -59,7 +100,8 @@ idx = tdm.sum(axis = 1).sort_values(ascending = False).index
 tdm = tdm.ix[idx] # sort the term-doc mat by word frequency
 totals = tdm.sum(axis = 1)
 
-# Below is a very heavy-handed way of making the network more manageable.  Just remove infrequent words.
+# Below is a very heavy-handed way of making the network more manageable.  
+# Just remove infrequent words.
 wordFreqThreshold = 30  
 tdm = tdm[totals > wordFreqThreshold] # remove rows for infrequent words
 totals = totals[totals > wordFreqThreshold]
@@ -80,16 +122,3 @@ pos = nx.spring_layout(G)
 nx.draw(G, node_size = 0, pos = pos, alpha = 0.04)  
 nx.draw_networkx_labels(G, pos = pos, font_color = '#2ca25f')
 plt.show() 
-
-
-
-
-
-
-
-
-
-
-
-
-

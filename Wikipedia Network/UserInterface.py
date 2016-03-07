b8d41d21d469c43942a022636e50ed317158a9e5
unknown <cbell75@N337ESC-08.physics.byu.edu>
2016-03-07 11:59:08 -0700

    Started the user interface

---
+++ Wikipedia Network/UserInterface.py
@@ -0,0 +1,6 @@
+## Simple terminal user interface
+## The user inputs a string of fundamental steps for a process
+## and the program finds similar articles in Wikipedia. These
+## articles are then suggested to the user.
+
+

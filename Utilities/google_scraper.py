4e8bacc9c9731d9da2116a25939565bd7738ce92
Jason Bell <j.jason.bell@gmail.com>
2016-05-23 11:49:08 -0500

    Another dummy commit to push

--- Utilities/google_scraper.py
+++ Utilities/google_scraper.py
@@ -8,8 +8,8 @@ import time
 BlenderPath = os.getenv('HOME') + '/Documents/Blender'
 
 start_time = time.time() # For timing 
-search_text = input(" What do you seek? ") 
-pages = int(input(" How many pages of results would you like? "))
+search_text = input("What do you seek? ") 
+pages = int(input("How many pages of results would you like? "))
 
 with open(BlenderPath + "/search_text.p", "wb") as f:
     pickle.dump(search_text, f)

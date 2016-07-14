3b25316593d025908d00cb1ce1b5c7b764ca58e4
Jason Bell <j.jason.bell@gmail.com>
2016-07-13 20:48:21 -0500

    Update list_schematic.py with better word2vec model and output formatting.  Still sucks, honestly.

--- Generators/list_schematic.py
+++ Generators/list_schematic.py
@@ -48,26 +48,25 @@ baseline = ksEvaluator(header, verbose = True)
 
 
 # Brute force-----------
-idea_list = []
-for item in candidates:
-    if item[1] == 'NN':
-        for target in header_tags:
-            if target[1] == 'NN':
-                #new_idea = item[0] + ' ' + target[0]
-                #new_idea = item[0] + ' ' + header 
-                new_idea = header.replace(target[0], item[0], 1)
-                idea_score = ksEvaluator(new_idea)
-                if idea_score < baseline:
-                    #new_idea_full = header.replace(target[0], item[0])
-                    new_idea_full = '%s is related to %s.  What if you replaced the idea of %s in %s with %s?' % (seed_term, target[0], target[0], seed_term, item[0]) 
-                    pair = (new_idea_full, idea_score)
-                    idea_list.append(pair)
-                    print(idea_score)
-
-ideas = list(set(idea_list))
-sorted_ideas = sorted(ideas, key = lambda tup: tup[1])
-ideas = [i for i in ideas if i[1] == sorted_ideas[0][1]]
-
+#idea_list = []
+#for item in candidates:
+#    if item[1] == 'NN':
+#        for target in header_tags:
+#            if target[1] == 'NN':
+#                #new_idea = item[0] + ' ' + target[0]
+#                #new_idea = item[0] + ' ' + header 
+#                new_idea = header.replace(target[0], item[0], 1)
+#                idea_score = ksEvaluator(new_idea)
+#                if idea_score < baseline:
+#                    #new_idea_full = header.replace(target[0], item[0])
+#                    new_idea_full = '%s is related to %s.  What if you #replaced the idea of %s in %s with %s?' % (seed_term, #target[0], target[0], seed_term, item[0]) 
+#                    pair = (new_idea_full, idea_score)
+#                    idea_list.append(pair)
+#                    print(idea_score)
+#
+#ideas = list(set(idea_list))
+#sorted_ideas = sorted(ideas, key = lambda tup: tup[1])
+#ideas = [i for i in ideas if i[1] == sorted_ideas[0][1]]
 
 
 
@@ -80,9 +79,11 @@ for i, sentence in enumerate(sentences):
     sentences[i] = text_fun.prune(temp, stem = False)
 mod = gensim.models.Word2Vec(sentences)
 
+
+mod = gensim.models.Word2Vec.load_word2vec_format(BlenderPath + '/Aux/deps.words.vector', binary = False)
+
 # If you're finished training the model
 mod.init_sims(replace = True)
-
 
 idea_list = []
 candidates = []
@@ -90,24 +91,26 @@ candidate_words = list(mod.vocab)
 cblob = TextBlob(' '.join(candidate_words), pos_tagger = PerceptronTagger())
 candidate_word_tags = cblob.tags
 for i, item in enumerate(candidate_words):
-    if candidate_word_tags[i][1] in ok_tags:
-        candidates.append(item)
+    if i < len(candidate_word_tags):
+        if candidate_word_tags[i][1] in ok_tags:
+            candidates.append(item)
 
-for candidate in candidates:
+for candidate in candidates[0:999]:
     if candidate in mod.vocab:
         sims = []
         for target in header_tags:
             if target[1] == 'NN' and target[0] in mod.vocab:
                 temp_tuple = (mod.similarity(target[0], candidate), target[0])
                 sims.append(temp_tuple)
-                if sims:
-                    best_tuple = min(sims, key = lambda t: t[0])
-                    new_idea = header.replace(best_tuple[1], candidate, 1)
-                    idea_score = ksEvaluator(new_idea)
-                    if idea_score < baseline:
-                        pair = (new_idea, idea_score)
-                        idea_list.append(pair)
-                        print(idea_score)
+        if sims:
+            best_tuple = max(sims, key = lambda t: t[0])
+            if best_tuple[1] not in seed_term and '-' not in candidate:
+                new_idea = header.replace(best_tuple[1], candidate)
+                idea_score = ksEvaluator(new_idea)
+                if idea_score < baseline:
+                    pair = (new_idea, idea_score)
+                    idea_list.append(pair)
+                    print(idea_score)
 
 ideas = list(set(idea_list))
 sorted_ideas = sorted(ideas, key = lambda tup: tup[1])

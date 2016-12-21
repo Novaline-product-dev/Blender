d9e93403f4b838e67875037e2b97cf4e7d56fb35
Jason Bell <j.jason.bell@gmail.com>
2016-12-21 12:43:44 -0600

    Drop creation of TFIDF corpus in HTML_2_Corpus.py, change paths (still hardcoded) to match Jason's iMac.

--- Wikipedia Network/HTML_2_Corpus.py
+++ Wikipedia Network/HTML_2_Corpus.py
@@ -34,29 +34,30 @@ def titlextractor(file_name):
 print(time(), 'Start.')
 # Provide a route by which to access all the files
 
-files = []
-# Make sure you have already generated a list of files with
-# filename_list_generator.py
-with open('../../Wikipedia Network/filenames.txt', 'r') as f:
-    for line in f:
-        files.append(line.strip('\n'))
-print(time(), 'Filenames Read.  For example, the first file is:', files[0])
-print('The current working directory is', os.getcwd())
-
-# Run through each file at a time, collecting stats about tokens
-# Weird list comprehension: http://stackoverflow.com/ ...
-# questions/1198777/double-iteration-in-list-comprehension
-# Try reading it from bottom to top
-print(time(), 'Beginning to load dictionary.')
-dictionary = corpora.Dictionary(
-    tf.prune(doc)
-        for file_name in files
-            for doc in textractor(file_name))
-print(time(), 'Dictionary loaded. Filtering extremes.')
-# Remove frequent and infrequent words, and limit tokens to 100,000
-dictionary.filter_extremes()
-dictionary.compactify()
-dictionary.save('dictionary.dict')
+#files = []
+## Make sure you have already generated a list of files with
+## filename_list_generator.py
+#with open('../../Wikipedia Network/filenames.txt', 'r') as f:
+#    for line in f:
+#        files.append(line.strip('\n'))
+#print(time(), 'Filenames Read.  For example, the first file is:', files[0])
+#print('The current working directory is', os.getcwd())
+#
+## Run through each file at a time, collecting stats about tokens
+## Weird list comprehension: http://stackoverflow.com/ ...
+## questions/1198777/double-iteration-in-list-comprehension
+## Try reading it from bottom to top
+#print(time(), 'Beginning to load dictionary.')
+#dictionary = corpora.Dictionary(
+#    tf.prune(doc)
+#        for file_name in files
+#            for doc in textractor(file_name))
+#print(time(), 'Dictionary loaded. Filtering extremes.')
+## Remove frequent and infrequent words, and limit tokens to 100,000
+#dictionary.filter_extremes()
+#dictionary.compactify()
+os.chdir('../wiki_model')
+#dictionary.save('wiki_dictionary.dict')
 
 
 #!!!!!!!  Dec 15, 2016
@@ -78,37 +79,56 @@ class MyCorpus(object):
                     f.write(''.join((title, '\n')))
                 yield dictionary.doc2bow(tf.prune(doc))
 
-print(time(), 'Building corpus.')
-corpus = MyCorpus() 
+#print(time(), 'Building corpus.')
+#corpus = MyCorpus() 
 
 # For debugging:
 ##for vector in corpus:
 ##    print(vector)
     
 # Convert the corpus to Market Matrix format and save it.
-print(time(), 'Corpus built. Converting to Market Matrix format.')
-corpora.MmCorpus.serialize('corpus.mm', corpus)
-print(time(), 'Market Matrix format saved. Process finished.')
+#print(time(), 'Corpus built. Converting to Market Matrix format.')
+#corpora.MmCorpus.serialize('wiki_corpus.mm', corpus)
+#print(time(), 'Market Matrix format saved. Process finished.')
+
+
+
+
+
 
 """The following will set everything up to use wikiScript to make comparisons.
-wikiScript.py requires a tfidfcorpus (corpus_tfidf.mm, see below),
-a dictionary (dictionary.dict, see above), and an lsi model
-(corpus_lsi.mm). This is probably best done from the shell as each of these
+wikiScript.py requires a tfidfcorpus (wiki_corpus_tfidf.mm, see below),
+a dictionary (wiki_dictionary.dict, see above), and an lsi model 
+(wiki.lsi). This is probably best done from the shell as each of these
 steps will take a very long time. However, this section can be uncommented out
 for testing on smaller sets of documents."""
 
 ### Load the Market Matrix corpus
-##mmcorpus = corpora.MmCorpus('corpus.mm')
-### Create the tfidf model object
-##tfidf = models.TfidfModel(mmcorpus)
-### Transform the whole corpus and save it
-##mmcorpus_tfidf = tfidf[mmcorpus]
-##corpora.MmCorpus.serialize('corpus_tfidf.mm', mmcorpus_tfidf)
-### Create the lsi model object
-##dictionary=corpora.Dictionary.load('dictionary.dict')
-##lsi = models.LsiModel(mmcorpus, id2word=dictionary, num_topics=2)
-##lsi.print_topics(2)
-### Transform the whole corpus and save it
-##mmcorpus_lsi = lsi[mmcorpus]
-##corpora.MmCorpus.serialize('corpus_lsi.mm', mmcorpus_lsi)
 
+mmcorpus = corpora.MmCorpus('wiki_corpus.mm')
+print('Wikipedia Corpus Loaded')
+
+#### Create the tfidf model object
+#print('Creating TFIDF model...')
+#tfidf = models.TfidfModel(mmcorpus) 
+#print('TFIDF model created')
+
+#print('Transforming corpus to TFIDF...')
+#### Transform the whole corpus and save it
+#mmcorpus_tfidf = tfidf[mmcorpus]
+#corpora.MmCorpus.serialize('corpus_tfidf.mm', mmcorpus_tfidf)
+#print('TFIDF corpus saved')
+
+print('Creating LSI Model...')
+#### Create the lsi model object
+dictionary=corpora.Dictionary.load('wiki_dictionary.dict')
+lsi = models.LsiModel(mmcorpus, id2word=dictionary, num_terms=100000, num_topics=400, decay=1.0, chunksize=20000)
+lsi.print_topics(2)
+lsi.save('wiki_lsi')
+
+
+#### Transform the whole corpus and save it
+print('Transforming Wikipedia Corpus to LSI')
+mmcorpus_lsi = lsi[mmcorpus]
+corpora.MmCorpus.serialize('wiki_corpus_lsi.mm', mmcorpus_lsi)
+print('Done!')

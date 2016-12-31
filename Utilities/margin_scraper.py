937cf3a5226827be563461ffffe2ccad7f45f791
Jason Bell <j.jason.bell@gmail.com>
2016-12-31 15:59:41 -0600

    Completed a working first version of margin_scraper.py.  It may crash if FINVIZ boots my IP address, but I am waiting for 5 seconds between each HTTP request in hopes that doesn't happen. We'll see.

--- Utilities/margin_scraper.py
+++ Utilities/margin_scraper.py
@@ -1,4 +1,5 @@
-import requests
+import requests, time
+import numpy as np
 from bs4 import BeautifulSoup as BS
 
 def get_industries():
@@ -22,11 +23,53 @@ def get_industries():
     inds = [el for el in inds if el[0] not in to_drop]
     return inds
 
-inds = get_industries()
+def get_tickers(ind):
+    ticker_url = 'http://finviz.com/screener.ashx?v=161&f=ind_' + \
+        ind + '&ft=2'
+    r = requests.get(ticker_url)
+    soup = BS(r.text, 'lxml')
+    td = soup('td', class_="screener-body-table-nw")
+    tickers = []
+    for tag in td:
+        ticker_string = tag.a['href']
+        ticker_string = ticker_string.replace('quote.ashx?t=', '')
+        ticker_string = ticker_string.replace('&ty=c&p=d&b=1', '')
+        tickers.append(ticker_string)
+    tickers = list(set(tickers))
+    return tickers
 
-url = 'http://finviz.com/screener.ashx?v=161&f=ind_' + \
-    inds[0][0] + '&ft=2'
-r = requests.get(url)
-soup = BS(r.text, 'lxml')
-td = soup('td', class_="screener-body-table-nw")
-# Next get tickers and look them up using FINVIZ HTTP syntax, scrape gross margin and place it in a list.
+def get_avg_margin(tickers):
+    margins = []
+    n_valid = 0
+    running_total = 0
+    for ticker in tickers:
+        print('Getting margin for', ticker)
+        time.sleep(5)
+        margin_url = 'http://finviz.com/quote.ashx?t=' + ticker + \
+            '&ty=c&p=d&b=1'
+        r = requests.get(margin_url)
+        soup = BS(r.text, 'lxml')
+        td = soup('td')
+        for i, tag in enumerate(td):
+            try:
+                if str(tag['title']).find('body=[Gross Margin (ttm)]') > 0:
+                    gm_loc = i + 1
+            except:
+                continue
+        margin = td[gm_loc].text
+        if margin != '-':
+            value = float(margin.replace('%', ''))
+            running_total += value
+            n_valid += 1
+    return running_total / n_valid
+
+inds = get_industries()
+for i, ind in enumerate(inds):
+    print('Current industry: ', ind[1])
+    tickers = get_tickers(ind[0])
+    print('Tickers for this industry:', tickers)
+    avg_margin = get_avg_margin(tickers)
+    inds[i] = (ind[0], ind[1], avg_margin)
+
+with open("gross_margins.p", "wb") as f:
+    pickle.dump(inds, f)

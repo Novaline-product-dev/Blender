de469c262179649d81708ff2d84f2b0067607a70
Jason Bell <j.jason.bell@gmail.com>
2017-01-10 19:52:37 -0600

    Started on operation.py for atotally new approach to idea generation.

--- Generators/operation.py
+++ Generators/operation.py
@@ -1,24 +1,25 @@
 import os
 os.chdir(os.getenv('HOME') + '/Documents/Blender')
+import random
 
-transformationList = [
-    'flip',
-    'rotation',
-    'resize',
-    'in vs. out',
-    'analog vs. digital',
-    '3D vs. 2D',
-    'push vs. pull',
-    'reemphasize' ,
-    'reparameterize',
-    'change of material',
-    'change of colors',
-    'opt in vs. opt out',
-    'change the height',
-    'flexibility vs. usability',
-    'form change',
-    'change in control mapping',
-    'fixed vs. variable',
-    'symmetric vs. asymmetric',
-    'conjunctive vs. disjunctive vs. compensatory',
-    'reorder']
+def operation(seed_term):
+    operations = [
+        'Flip %s vertically, horizontally, or inside out.' %seed_term,
+        'Rotate %s in physical or abstract space.' %seed_term,
+        'Resize %s (What about a giant version or a tiny one?)' %seed_term,
+        'Change %s from analog to digital or vice versa.' %seed_term,
+        'Change %s from 3D to 2D or vice-versa.' %seed_term,
+        'Change the focal point of %s (perhaps to something that was previously ignored.)' %seed_term,
+        'Change %s\'s method of control. (Ex: from text to voice control.)' %seed_term,
+        'change the material (Ex: from wood to steel, from Objective-C to Python.)',
+        'change the colors',
+        'make the product from opt in to opt out or vice-versa.',
+        'change the height',
+        'flexibility vs. usability',
+        'form change',
+        'change in control mapping',
+        'fixed vs. variable',
+        'symmetric vs. asymmetric',
+        'conjunctive vs. disjunctive vs. compensatory',
+        'reorder']
+    return random.choice(operations)

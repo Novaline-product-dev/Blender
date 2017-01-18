9ef6077cf2d304af32bc7ac5268591320ec007d0
Jason Bell <j.jason.bell@gmail.com>
2017-01-18 11:27:59 -0600

    Improving operation.py

--- Generators/operation.py
+++ Generators/operation.py
@@ -3,6 +3,27 @@ os.chdir(os.getenv('HOME') + '/Documents/Blender')
 import random
 from nltk.corpus import wordnet as wn
 
+def audience():
+    alts = [
+        'men',
+        'women',
+        'healthcare professionals',
+        'runners',
+        'creatives',
+        'businesspeople',
+        'parents',
+        'kids',
+        'the disabled',
+        'students',
+        'the wealthy'
+        'the elderly',
+        'Millenials',
+        'Baby Boomers',
+        'people from Generation X',
+        'busy people',
+        'obese people'
+    ]
+    return random.choice(alts)
 
 def material():
     choices = ['acrylic', 'alloy', 'aluminum', 'brass',
@@ -15,75 +36,105 @@ def material():
     return random.choice(choices)
 
 def process():
-    processes = ['design', 'purchase', 'maintenance', 'core function', 'auxilliary functions']
-    return random.choice(processes)
+    alts = ['design', 'purchase', 'maintenance', 'core function', 'auxilliary functions']
+    return random.choice(alts)
     
-def business_model():
-    b_mods = ['Allow customers to name a price (ex: Priceline)',
-    'Use offer aggregation (ex: Kayak, eBay, Amazon)',
-    'Change to just-in-time production, or the pull method (ex: Dell, Toyota)',
-    'Growth first, profit later (ex: Amazon, many startups)',
-    'The modern franchise (ex: Macdonalds)',
-    'Software first, hardware to follow (ex: Microsoft, Google, Amazon)',
-    'Walled-garden product line (ex: Apple, Google)',
-    'The P2P Revolution (ex: Etsy, Kickstarter, Paypal, Craigslist',
-    'Vertical integration (ex: Tesla)',
-    'Advertiser model: if you drive traffic, sell it (ex: Google, FB)', 
-    'Data model: gather info and sell access (ex: Bloomberg)',
-    'Merchant model: resell (ex: TJMaxx)',
-    'Brokerage model: sell knowledge about an activity/service (ex: Goldman Sachs)',
-    'Affiliate/commision model: take a cut of sales you create (ex: Kickstarter)',
-    'Auction model: gather buyers and sellers in your industry (ex: eBay, Amazon)',
-    'Add-on Model: razor and blades or upgrades (ex: Gillette, Super Mario Run)',
-    'Direct Sales, no middle retailer (ex: Warby-Parker)',
-    'Freemium or try-before-you-buy (ex: AWS Free Tier)',
-    'Low-cost + upsell (ex: Ryanair)',
-    'Pay-as-you-go (ex: Utilities, AWS)',
-    'All You Can Use/Eat (ex: Noun Project Pro)',
-    'Crowdsource a new launch (ex: Pebble)',
-    'Price by algorithm (ex: Uber, Logo Pizza, Jet)']
+def business_model(seed_term):
+    b_mods = ['allowing customers to name their own price for %ss' %seed_term,
+    'using offer aggregation to sell %ss (e.g. Kayak, eBay, Amazon)' %seed_term,
+    'changing to just-in-time production for %ss' %seed_term,
+    'building a %s franchise' %seed_term,
+    'software first, hardware to follow (ex: Microsoft, Google, Amazon)',
+    'walled-garden product line (ex: Apple, Google)',
+    'the P2P Revolution (ex: Etsy, Kickstarter, Paypal, Craigslist',
+    'vertically integrating %s production' %seed_term,
+    'gather %s-related information and sell access to it' %seed_term,
+    'merchant model: resell (ex: TJMaxx)',
+    'sell knowledge about an activity/service (ex: Goldman Sachs)',
+    'auction %ss',
+    'add-on Model: razor and blades or upgrades (ex: Gillette, Super Mario Run)',
+    'direct Sales, no middle retailer (ex: Warby-Parker)',
+    'freemium or try-before-you-buy (ex: AWS Free Tier)',
+    'low-cost + upsell (ex: Ryanair)',
+    'pay-as-you-go (ex: Utilities, AWS)',
+    'all You Can Use/Eat (ex: Noun Project Pro)',
+    'price by algorithm (ex: Uber, Logo Pizza, Jet)']
     return random.choice(b_mods)
 
-def opposites():
-    alts = [
-        ['bumpy/rough', 'smooth'],
+def adjectives(seed_term):
+    adjs = [
+        'more reliable',
+        'more authentic',
+        'more confident',
+        'brighter',
+        'cleaner',
+        'more social',
+        'more portable',
+        'lighter',
+        'more secure',
+        'autonomous'
+    ]
+    return random.choice(adjs)
+
+def opposites(seed_term):
+    alts = [['bumpy/rough', 'smooth'],
         ['analog', 'digital'],
         ['3D', '2D'],
         ['opaque', 'transparent'],
         ['opt-in', 'opt-out'],
         ['symmatric', 'assymetric'],
-        ['temporary', 'durable']
-    ]
-    return random.choice(random.choice(alts))
+        ['temporary', 'durable'],
+        ['minimalist', 'feature-rich'],
+        ['powerful', 'efficient'],
+        ['exclusive', 'accessible']]
+    alt = random.choice(alts)
+    idea = 'Make %ss more %s or more %s.' %(seed_term, alt[0], alt[1])
+    return idea
 
-def operation(seed_term):
-    operations = [
-        'Flip the %ss vertically, horizontally, or inside out.' %seed_term,
-        'Rotate the %s in physical or abstract space.' %seed_term,
-        'Resize the %s (A giant version? A tiny one?)' %seed_term,
-        'Change distribution of %ss from inventory to made-to-order or vice-versa.' %seed_term,
-        'Change the %s from analog to digital or vice versa.' %seed_term,
-        'Change the focal point of %ss (perhaps to something that was previously ignored.)' %seed_term,
-        'Change the method of control for the %s.' %seed_term,
-        'Change the material or building blocks of the %s.' %seed_term,
-        'Change the colors, opacity, or brightness of the %s.' %seed_term,
-        'Make %s much taller or much shorter' %seed_term,
-        'Sacrifice features of %s for improved usability or add features to %s for improved power.' %(seed_term, seed_term),
-        'Change the delivery format of %s.' %seed_term,
-        'Change the order in which users interact with %s' %seed_term,
-        'Allow consumers to spread purchase costs of %s across time or crowds.' %seed_term,
-        'Make %s more portable.' %seed_term,
-        'Build a platform where users can create their own %s' %seed_term,
-        'Change the source of revenue from %s.' %seed_term,
-        'Make %s more social.' %seed_term,
-        'Allow consumers to communicate with %s or about %s' %(seed_term, seed_term),
-        'Provide customized business services for the %s industry.' %seed_term,
-        'Decrease the production time or increase the production quality of %s.' %seed_term,
-        'Make %s easier to maintain.' %seed_term,
-        'Make %s more exclusive or more accessible.' %seed_term,
-        'Change the target audience of %s to one that seeks a different benefit.' %seed_term,
-        'Remove an attribute from %s' %seed_term,
-        'Automate the %s of %s' %(process(), seed_term),
-        'For %s, experiment with this business model: %s' %(seed_term, business_model())
+def transformation(seed_term):
+    transformed = [
+    'How about a %s that is horizantally reversed?' %seed_term,
+    'Try a %s that is vertically flipped.' %seed_term,
+    'Make a %s that is inside out.' %seed_term,
+    'A giant %s!' %seed_term,
+    'A tiny %s, maybe for a new purpose.' %seed_term
+    ]
+    return random.choice(transformed)
+
+def platform_functions(seed_term):
+    funs = [
+        'Build a platform where users can create their own %s.' %seed_term,
+        '''Build a platform where people communicate 
+        with each other using %ss or about %ss.''' %(seed_term, seed_term),
+        'Build a platform where users can buy and sell %ss.' %seed_term
+    ]
+    return random.choice(funs)
+
+def control_types():
+    types = [
+        'with software',
+        'using an algorithm',
+        'with voice',
+        'using touch',
+        'with a stylus',
+        'via a command line',
+        'remotely'
+    ]
+    return random.choice(types)
+
+def template(seed_term):
+    templates = [
+        transformation(seed_term),
+        opposites(seed_term),
+        'Allow users to control %ss %s.' %(seed_term, control_types()),
+        'Literally or figuratively, make a %s %s.' %(material(), seed_term),
+        'Make a %s that is %s.' %(seed_term, adjectives(seed_term)),
+        platform_functions(seed_term),
+        'Make a %s for %s.' %(seed_term, audience()),
+        'Allow users to automate the %s of %ss.' %(process(), seed_term),
+        'Experiment with %s.' %business_model(seed_term)
         ]
-    return random.choice(operations)
+    return random.choice(templates)
+
+for i in range(0, 10):
+    print('\n', template('notebook'))

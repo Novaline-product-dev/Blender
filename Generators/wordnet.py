b2cf44ad59642ad15ec059330730045634e1b881
Jason Bell <j.jason.bell@gmail.com>
2017-01-17 22:35:10 -0600

    Still experimenting with wordnet, this time expanding to cousin and second-cousin terms and then getting meronyms.

--- Generators/wordnet.py
+++ Generators/wordnet.py
@@ -14,81 +14,53 @@ goog_list = pickle.load(open('fulltext.p', 'rb'))
 text_list = [text_fun.prune(doc) for doc in goog_list]
 ksEvaluator = ksmirnov_fun.ksFunctionGenerator(text_list)
 article = wikipedia.page(seed_term).content
-def get_names(synlist):
-    out = []
-    for el in synlist:
-        long_name = el.name()
-        to_add = long_name[:long_name.find('.')]
-        out.append(to_add)
-    return out
-
 refs = []
 seed_term = seed_term.split()[len(seed_term.split()) - 1]
-synset = wn.synsets(seed_term)[0]
-hyper3nyms = synset.hypernyms()[0].hypernyms()[0].hypernyms()[0]
-for el in hyper3nyms.hyponyms():
-    hypernyms = el.hyponyms()
-    for item in hypernyms:
-        refs.extend(get_names(hypernyms))
-print('line 32')
-article = article.replace('\n', ' ')
-article = article.replace('=', '')
-aug = article.split()
-candidates = []
-for text in text_list:
-    candidates.extend(text)
-candidates = list(set(candidates))
-new_ideas = []
-for candidate in candidates:
-    temp = list(aug)
-    temp.append(candidate)
-    score = ksEvaluator(temp)
-    print(score)
-    tup = (candidate, score)
-    new_ideas.append(tup)
+hyper = lambda s: s.hypernyms()
+hypo = lambda s: s.hyponyms()
+meronyms = []
+seed_synset = wn.synsets(seed_term)[0]
+seed_hypers = list(seed_synset.closure(hyper, depth=4))
+for seed_hyper in seed_hypers:
+    for el in list(seed_hyper.closure(hypo, depth=4)):
+        el_meros = el.part_meronyms()
+        if el_meros:
+            name = el.lemmas()[0].name()
+            tup = (name, el_meros)
+            meronyms.append(tup)
+seen = set()
+meros = []
+for el in meronyms:
+    if el[0] not in seen:
+        meros.append(el)
+        seen.add(el[0])
 
-new_ideas = sorted(new_ideas, key=lambda x:x[1])    
-for new_idea in new_ideas:
-    print('Blend a %s and a %s.  Score: %d' \
-          %(seed, new_ideas[0], new_ideas[1]))
-#def get_defns(seed):
-#    seed = wn.synsets(seed, pos=wn.NOUN)[0]
-#    defns = []
-#    defns.append(seed.definition())
-#    hypos = seed.hyponyms()
-#    if hypos:
-#        for nym in hypos:
-#            defns.append(nym.definition())
-#    blob = TextBlob(' '.join(defns),
-#                    pos_tagger = PerceptronTagger())
-#    blob = list(set(blob.tags))
-#    blob = [tag[0] for tag in blob if tag[1] == 'NN']
-#    blob = [w for w in blob if w not in set([')', '('])]
-#    return blob, defns
-#
-#def sister_terms(synset):
-#    out = []
-#    for hypernym in synset.hypernyms():
-#        hyponyms = hypernym.hyponyms()
-#        for hyponym in hyponyms:
-#            name = hyponym.name()
-#            out.append(name[:name.find('.')])
-#    return out
-#
-#def defn_expand(defn, blob):
-#    ideas = []
-#    for item in blob:
-#        if defn.find(item) != -1:
-#            wn_item = wn.synsets(item)[0]
-#            sisters = sister_terms(wn_item)
-#            for sister in sisters:
-#                if sister != item:
-#                    to_add = defn.replace(item, 
-#                        sister)
-#                    ideas.append(to_add)
-#    return ideas
-#
-#blob, defns = get_defns('notebook')
-#new_ideas = []
-#for defn in defns:
-#    new_ideas.extend(defn_expand(defn, blob))#
+for el in meros:
+    print(el, '\n')
+
+new_ideas = []
+for el in meros:
+    new_ideas.append('Blend a %s and a %s' %(seed_term, el[0]))
+    for meronym in el[1]:
+        name = meronym.lemmas()[0].name()
+        idea = 'Use the %s from a %s to make a new %s' %\
+            (name, el[0], seed_term)
+        new_ideas.append(idea)
+
+for idea in new_ideas:
+    print(idea, '\n')
+
+
+#new_ideas2 = []
+#defn = seed_synset.definition()
+#defn_tags = TextBlob(defn, pos_tagger = PerceptronTagger()).tags
+#defn_tags = [el[0] for el in defn_tags if el[1] in set(['NN', 'NNS'])]
+#for target in defn_tags:
+#    target_synset = []
+#    target_synset = wn.synsets(target)
+#    if target_synset:
+#        target_syn = target_synset[0]
+#        target_sisters = target_syn.hypernyms()[0].hyponyms()
+#        for sister in target_sisters:
+#            sister_name = sister.lemmas()[0].name()
+#            new_ideas2.append(defn.replace(target, sister_name))

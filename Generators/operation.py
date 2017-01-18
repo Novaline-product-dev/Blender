cdbe857cd6820a9f6b409a6d5b45ca428c0717a5
Jason Bell <j.jason.bell@gmail.com>
2017-01-18 09:42:05 -0600

    Added opposites function to operation.py

--- Generators/operation.py
+++ Generators/operation.py
@@ -44,26 +44,32 @@ def business_model():
     'Price by algorithm (ex: Uber, Logo Pizza, Jet)']
     return random.choice(b_mods)
 
+def opposites():
+    alts = [
+        ['bumpy/rough', 'smooth'],
+        ['analog', 'digital'],
+        ['3D', '2D'],
+        ['opaque', 'transparent'],
+        ['opt-in', 'opt-out'],
+        ['symmatric', 'assymetric'],
+        ['temporary', 'durable']
+    ]
+    return random.choice(random.choice(alts))
+
 def operation(seed_term):
     operations = [
-        'Change %s from rough/bumpy to smooth or vice-versa.' %seed_term,
-        'Flip %s vertically, horizontally, or inside out.' %seed_term,
-        'Rotate %s in physical or abstract space.' %seed_term,
-        'Resize %s (A giant version? A tiny one?)' %seed_term,
-        'Scale production of %s way up or way down.' %seed_term,
-        'Change distribution of %s from inventory to made-to-order or vice-versa.' %seed_term,
-        'Change %s from analog to digital or vice versa.' %seed_term,
-        'Change %s from 3D to 2D or vice-versa.' %seed_term,
-        'Change the focal point of %s (perhaps to something that was previously ignored.)' %seed_term,
-        'Change the method of control for %s.' %seed_term,
-        'Change the material or building blocks of %s.' %seed_term,
-        'Change the colors, opacity, or brightness of %s.' %seed_term,
-        'Change %s from opt in to opt out or vice-versa.' %seed_term,
+        'Flip the %ss vertically, horizontally, or inside out.' %seed_term,
+        'Rotate the %s in physical or abstract space.' %seed_term,
+        'Resize the %s (A giant version? A tiny one?)' %seed_term,
+        'Change distribution of %ss from inventory to made-to-order or vice-versa.' %seed_term,
+        'Change the %s from analog to digital or vice versa.' %seed_term,
+        'Change the focal point of %ss (perhaps to something that was previously ignored.)' %seed_term,
+        'Change the method of control for the %s.' %seed_term,
+        'Change the material or building blocks of the %s.' %seed_term,
+        'Change the colors, opacity, or brightness of the %s.' %seed_term,
         'Make %s much taller or much shorter' %seed_term,
         'Sacrifice features of %s for improved usability or add features to %s for improved power.' %(seed_term, seed_term),
         'Change the delivery format of %s.' %seed_term,
-        'Change %s from symmetric to asymmetric or vice-versa.' %seed_term,
-        'Change %s from all-or-nothing to anything counts or vice-versa.' %seed_term,
         'Change the order in which users interact with %s' %seed_term,
         'Allow consumers to spread purchase costs of %s across time or crowds.' %seed_term,
         'Make %s more portable.' %seed_term,
@@ -73,7 +79,6 @@ def operation(seed_term):
         'Allow consumers to communicate with %s or about %s' %(seed_term, seed_term),
         'Provide customized business services for the %s industry.' %seed_term,
         'Decrease the production time or increase the production quality of %s.' %seed_term,
-        'Change %s from disposable to durable or vice-versa.' %seed_term,
         'Make %s easier to maintain.' %seed_term,
         'Make %s more exclusive or more accessible.' %seed_term,
         'Change the target audience of %s to one that seeks a different benefit.' %seed_term,

717f4cb59e04ef84cf3420c30f8df23277422911
Jason Bell <j.jason.bell@gmail.com>
2017-01-31 16:39:48 -0600

    Improvements on wikilinks.  I've noticed that the seed term really matters.  Complexity and newness both seem to influence how good the output is.

--- generators/wikilinks.py
+++ generators/wikilinks.py
@@ -1,13 +1,29 @@
 import wikipedia
+import random
 from spacy.en import English
 from spacy.symbols import amod
 
 
 nlp = English()
-a = wikipedia.page('pen')
-doc = a.content
+seed = 'skateboard'
+page = wikipedia.page(seed)
+seed_word = nlp(seed)[0]
+doc = page.content
+doc_split = doc.split('\n\n\n=')
+doc = doc_split[0]
+for sec in doc_split[1:]:
+    sec_header = sec[:sec.find('\n') + 1]
+    if sec_header.find('History') == -1:
+        sec_header = sec_header.replace('=', '')
+        sec_header = sec_header.strip()
+        clean = sec[sec.find('\n') + 1:].strip()
+        print(sec_header)
+        doc = doc + ' ' + sec_header + ' ' + clean
+doc = doc.replace('=', ' ')
+doc = doc.replace('References', '')
+doc = doc.replace('\n', ' ')
+doc = ' '.join(doc.split())
 doc = nlp(doc)
-pen = nlp('pen')[0]
 
 def get_adj_phrases(doc, word):
     out = []
@@ -17,18 +33,20 @@ def get_adj_phrases(doc, word):
                 out.append(str(el) + ' ' + str(el.head))
     return out
 
-links = [l for l in a.links if pen.similarity(nlp(l)[0]) < 0.3]
+
+links = [l for l in page.links if seed_word.similarity(nlp(l)[0]) > 0.2]
+links = random.sample(links, min(10, len(links)))
 mondo = []
 for l in links:
     try:
         link_doc = wikipedia.page(l).summary
         link_doc = nlp(link_doc)
-        to_add = get_adj_phrases(link_doc, pen)
+        to_add = get_adj_phrases(link_doc, seed_word)
         mondo.extend(to_add)
-        to_print = 'links from %s added.  Ex: A pen with %s' %(l, to_add[0])
+        to_print = 'links from %s added.  Ex: A %s with %s' %(l, seed_word, to_add[0])
         print(to_print)
     except:
         continue
 
 for el in mondo:
-    print('A pen with %s' %el)
+    print('A %s with %s' %(seed_word, el))

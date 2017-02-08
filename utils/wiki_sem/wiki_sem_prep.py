65dd4c75dbcc210afdaad72a9a6c742d2b1256ee
jjasonbell <j.jason.bell@gmail.com>
2017-02-08 12:47:30 -0600

    Add code to train semantic word2vec model and save it

--- utils/wiki_sem/wiki_sem_prep.py
+++ utils/wiki_sem/wiki_sem_prep.py
@@ -1,6 +1,7 @@
 import os
 os.chdir(os.getenv('HOME') + '/Documents/Blender')
 import gzip
+import shutil
 from utils import text_fun 
 from gensim import corpora, models, similarities
 
@@ -18,15 +19,15 @@ folders = ['../wiki_html/' + f for f in \
 # process files 
 for folder in folders:
     folder_files = os.listdir(folder)
-    folder_files = [f for f in folder_files if not \
-        f.startswith('.')]
+    folder_files = [f for f in folder_files if \
+        f.startswith('wiki_')]
     for file in folder_files:
         input_path = folder + '/' + file
         sentenc_path = folder + '/' + 'dependency_sentences_' + file
         text_fun.prep_save_sem(input_path, sentenc_path)
         print('%s/%s processed.' %(folder, file))
 
-if os.path.isfile(sentences_path):
+if os.path.isfile(sentences_path + '.gz'):
     print('Dependency sentences file found on disk at', )
 else:
     with gzip.open(sentences_path + '.gz', 'wb') as f:
@@ -39,3 +40,8 @@ else:
                 to_add = folder + '/' + file
                 with open(to_add, 'rb') as f_sub:
                     shutil.copyfileobj(f_sub, f) 
+
+sentences = models.word2vec.LineSentence(sentences_path + '.gz')
+sem_model = models.word2vec.Word2Vec(sentences, size=300, window=5, 
+    min_count=8, workers=3, sg=1, negative=10)
+sem_model.save('wiki_sem_w2v')

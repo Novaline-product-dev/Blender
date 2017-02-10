d83af3b0f157ed81f134e4b0f75337657d0a2efd
jjasonbell <j.jason.bell@gmail.com>
2017-02-10 14:49:05 -0600

    Created a new, rather poor idea generator.  It doesn't work well.

---
+++ generators/startup_mashup.py
@@ -0,0 +1,30 @@
+import requests
+import random
+from bs4 import BeautifulSoup as BS 
+
+
+url = 'http://www.startupranking.com/top/united-states'
+r = requests.get(url)
+soup = BS(r.content, 'lxml')
+soup = soup.tbody
+soup_names = soup.find_all(class_ = 'name')
+names = []
+links = []
+descriptions = []
+for tag in soup_names[0:30]:
+	url_end = tag.a['href']
+	spec_page = requests.get('http://www.startupranking.com' + url_end)
+	spec_soup = BS(spec_page.content, 'lxml')
+	su_info = spec_soup.find_all(class_ = 'su-info')
+	raw_description = su_info[0].find_all('p')[0].get_text()
+	raw_description = raw_description.replace('...\nSee More\n', '')
+	raw_description = raw_description.replace('\n', '')
+	descriptions.append(raw_description.strip())
+	name = tag.a.get_text()
+	names.append(name)
+	print('Finished with %s' %name)
+
+locs = random.sample(range(0, 30), 2)
+print('Combine %s and %s' %(names[locs[0]], names[locs[1]]))
+print('Description for ', names[locs[0]], ': ', descriptions[locs[0]])
+print('Description for ', names[locs[1]], ': ', descriptions[locs[1]])

2f2cc8a97771818913d24c70b17973fafba5dab5
jjasonbell <j.jason.bell@gmail.com>
2017-03-04 11:15:22 -0600

    Export YC data as json

--- generators/yc_template.py
+++ generators/yc_template.py
@@ -1,6 +1,6 @@
 import os, sys, pickle
 os.chdir(os.getenv('HOME') + '/Documents/Blender')
-from utils import spacy_analogy
+from utils import yc_data_gen
 from numpy.random import choice
 import spacy
 import requests
@@ -14,178 +14,14 @@ from bs4 import BeautifulSoup as BS
 if os.path.isfile('yc_objs.p'):
 	with open('yc_objs.p', 'rb') as f:
 		objs = pickle.load(f)
-	descriptions = objs[0]
-	targets = objs[1]
-	ranks1 = objs[2]
-	ranks2 = objs[3]
 else:
-	nlp = spacy_analogy.nlp
-	r = requests.get('http://yclist.com')
-	soup = BS(r.text, 'lxml')
-	tbody = soup.tbody
-	cos = tbody('tr')
-	data_list = [] 
-	for co in cos:
-	    tds = co('td')
-	    status = co['class'][0]
-	    if status != 'dead':
-	        des_el = tds[5]
-	        span_list = des_el('span')
-	        if span_list:
-	            span_list[0].extract()
-	        data_list.append({
-	            'Name': tds[1].text.strip(),
-	            'URL': tds[2].text.strip(),
-	            'Class': tds[3].text.strip(), 
-	            'Status': co['class'][0],
-	            'Description': des_el.text.strip()
-	            })
-	yclist = pd.DataFrame(data_list)
-
-	# could parse with spacy and use appropriate dependencies
-	# to remove bad targets.  Currently it's manual :(
-	skip = ['hykso', 'meter feeder', 'nurx', 'goodybag',
-		'perlara', 'petcube', 'trac', 'worldcover', 
-		'flexreceipts', 'pakible', 'paperspace', 'l. international',
-		'paribus', 'onboardiq', 'seva coffee', 'redcarpetup',
-		'physiohealth', 'greenshoe', 'mtailor', 'shout', 
-		'taplytics', 'airbnb', 'agilediagnosis', 'auctomatic',
-		'clearbanc', 'panorama education', 'circuitlab',
-		'inklingmarkets', 'careskore', 'the flex company',
-		'compose', 'isono health', 'givemetap']
-	targets = []
-	for i, row in yclist.iterrows():
-		description = row['Description'].lower()
-		name = row['Name'].lower()
-		if (description.find(name) == -1) and (name not in skip): 
-		    for_ind = description.find('for ')
-		    if for_ind != -1:
-		        end = description.find('.')
-		        if end == -1:
-		            targets.append(description[for_ind + 4:])
-		        else:
-		            targets.append(description[for_ind + 4:end])
-
-	drop = ['for you', 'for all']
-	targets = [t for t in targets if len(nlp(t)) < 6]
-	targets = [t for t in targets if t not in drop]
-	targets = [t for t in targets if t != 'for you']
-	targets = [t for t in targets if t.find('your ') == -1]
-	targets = [t for t in targets if t]
-	targets = list(set(targets))
-	[print(t) for t in targets]
-
-	targets2 = [
-		# misc
-		'parking', 'ordering food', 'rare genetic diseases',
-		'athletic events', 'crop insurance', 'insurance', 
-		'hygiene', 'groceries', 'apparel', 'furniture',
-		'appliances', 'packagine', 'cloud computing',
-		'parallel processing', 'research labs', 'machine shops',
-		'workforce management', 'scheduling', 'resource scheduling',
-		'online shopping', 'custom apparel', 'marketplaces',
-		'living spaces', 'hotels', 'real estate', 'education',
-		'data analytics', 'the self-employed', 'contractors',
-		'freelancers', 'engineers', 'students', 'design', 
-		'circuits', 'prediction markets', 'hospitals', 'newspapers',
-		'cancer detection',
-		# energy
-		'energy', 'solar energy', 'wind energy', 'nuclear energy', 
-		'natural gas', 'hydroelectric energy', 'batteries', 
-		'grid scale energy', 'energy transmission', 
-		'renewable energy',	
-		# AI
-		'artificial intelligence', 'search', 
-		'automated reasoning', 'machine learning', 
-		'neural networks', 'machine translation',
-		# robotics
-		'robotics', 'industrial robots', 'medical robots',
-		'educational robots', 'construction robots', 
-		# biotech
-		'biotechnology', 'agricultural technology', 
-		'bioinformatics', 'genomics', 'genetic engineering',
-		# healthcare
-		'healthcare',
-		# pharmaceuticals
-		'pharmaceuticals',
-		# education
-		'education', 
-		# human augmentation
-		'human augmentation', 'reproductive technology',
-		'plastic surgery', 'orthodontics', 'prosthetics',
-		'medical implants', 'nootropics', 'gene therapy',
-		# virtual/augmented reality
-		'VR/AR',
-		# transportation
-		'transportation', 
-		# housing
-		'housing', 
-		# programming tools
-		'programming tools', 
-		# peer-to-peer entertainment
-		'peer-to-peer entertainment', 
-		# enterprise software
-		'enterprise software', 
-		# finincial services
-		'financial services', 
-		# computer security
-		'computer security', 
-		# global health
-		'global health', 
-		# underserved communities
-		'underserved communities', 
-		# food and farming
-		'food and farming', 
-		# mass media
-		'mass media',
-		# news
-		'news',
-		# water
-		'water purification', 'irrigation', 
-		'water use effeciency', 
-		'water storage', 'water transportation'
-	]
-
-	def get_alexa_rank(url):
-	    url = "http://data.alexa.com/data?cli=10&dat=s&url=" + url
-	    r = requests.get(url)
-	    soup = BS(r.content, "lxml")
-	    reach = soup.find('reach')
-	    if reach:
-	        rank = reach['rank']
-	    else:
-	        rank = -1
-	    return rank
-
-	names = []
-	ranks = []
-	descriptions = []
-	for i in range(0, yclist.shape[0]):
-	    row = yclist.iloc[i]
-	    rank_i = get_alexa_rank(row['URL'])
-	    print('Got rank for %s' %row['Name'])
-	    ranks.append((row['Name'], rank_i))
-	    descriptions.append((row['Name'], yclist.iloc[i]['Description']))
-	    names.append(yclist['Name'])
-
-	ranks = [(r[0], int(r[1])) for r in ranks]
-	ranks = [r for r in ranks if r[1] != -1]
-	ranks = sorted(ranks, key = lambda x: x[1])
-	ranks2 = [r[1] for r in ranks]
-	max_rank = max(ranks2) + 1
-	ranks2 = [(max_rank - r) for r in ranks2]
-	ranks_sum = sum(ranks2)
-	ranks2 = [r / ranks_sum for r in ranks2]
-	ranks1 = [r[0] for r in ranks]
-	targets.extend(targets2)
-
-	objs = []
-	objs.append(descriptions)
-	objs.append(targets)
-	objs.append(ranks1)
-	objs.append(ranks2)
-	with open('yc_objs.p', 'wb') as f:
-		pickle.dump(objs, f)
+	yc_data_gen.yc_scrape()
+	with open('yc_objs.p', 'rb') as f:
+		objs = pickle.load(f)
+descriptions = objs[0]
+targets = objs[1]
+ranks1 = objs[2]
+ranks2 = objs[3]
 
 def generate():
     entity = choice(ranks1, 1, p=ranks2)[0]
